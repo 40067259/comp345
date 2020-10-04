e441f13f035c2e49a6f1f4e0b7f0f0afee808eaa
rileyelirileyelir <69334895+rileyelirileyelir@users.noreply.github.com>
2020-10-04 18:01:36 -0400

    Delete Strategy.cpp

--- Warzone/Strategy.cpp
+++
@@ -1,9 +0,0 @@
-#include "Strategy.h"
-
-void Strategy::reinforcement(Player* player) {}
-
-void Strategy::attack(Player* player) {}
-
-void Strategy::fortify(Player* player) {}
-
-Strategy::~Strategy() {}
