2865e75afac6d7ca5e5c56e25fdb0a29345f6317
800bb <65259687+800bb@users.noreply.github.com>
2020-10-07 19:38:42 -0400

    Add files via upload

---
+++ Map/Continent.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+#include "Map.h"
+
+class Territory;
+//class Player;
+struct Node;
+
+class Continent 
+{
+public:
+	Continent(std::string name, int nativeArmy);
+	~Continent();
+	int getNativeArmy() const;
+	void addTerritory(Node& Territory);
+	std::string getName() const;
+	void printTerritory();
+	//bool owner(const Player* player);
+	std::vector<Node*> getTerritories() const;
+
+private:
+	int nativeArmy;
+	std::string continentName;
+	std::vector<Node*> territories;
+};
