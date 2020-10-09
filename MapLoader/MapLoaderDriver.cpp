307ae15c3965faff1195d3cd2bc19b3c757b7209
rileyelirileyelir <69334895+rileyelirileyelir@users.noreply.github.com>
2020-10-09 16:46:26 -0400

    Update MapLoaderDriver.cpp

--- MapLoader/MapLoaderDriver.cpp
+++ MapLoader/MapLoaderDriver.cpp
@@ -8,6 +8,8 @@ using namespace std;
 int main() {
 	MapLoader map;
 	map.loadMap("estonia.map");
+	//map.loadMap("france.map");
+	//map.loadMap("google.map");
 	map.printMapInfo();
 	cout << endl;
 	map.printMap();
