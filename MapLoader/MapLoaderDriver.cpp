d9ad41542a3e7a7469366d6361e2418f42de8bb0
rileyelirileyelir <69334895+rileyelirileyelir@users.noreply.github.com>
2020-10-12 17:26:49 -0400

    Update MapLoaderDriver.cpp

--- MapLoader/MapLoaderDriver.cpp
+++ MapLoader/MapLoaderDriver.cpp
@@ -1,18 +1,25 @@
-#include <iostream>
 #include "Map.h"
 #include "MapLoader.h"
+#include <iostream>
 #include <string>
 #include <fstream>
+using std::cout;
 
 int main() {
+
 	MapLoader map;
-	map.loadMapAlt("google.map");
+	map.loadMap("google.map");
 	//map.loadMap("france.map");
 	//map.loadMap("estonia.map");
+
+	cout << "\n";
+
 	map.printMapInfo();
-	std::cout << std::endl;
-	map.printMap();
-	std::cout << std::endl;
+	cout << std::endl;
 	
+	map.printMap();
+	cout << std::endl;
+
 	system("pause");
+	
 }
