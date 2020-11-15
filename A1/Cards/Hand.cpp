277dbf68df80522132b31fed62e73521df15ecb7
Arash Manpreet Singh <singharash09@gmail.com>
2020-11-15 09:35:21 -0500

    fixed memory leak on hand deconstructor

--- A1/Cards/Hand.cpp
+++ A1/Cards/Hand.cpp
@@ -9,46 +9,45 @@
 
 Hand::Hand()
 {
-	hand = new std::vector<Card>;
+    hand = new vector<Card>;
 }
 
 //copy constructor
 Hand::Hand(const Hand& h)
 {
-	hand = new std::vector<Card>();
-	for (int i = 0; i < h.hand->size(); i++)
-	{
-		hand->emplace(hand->begin() + i, h.hand->at(i).getType()); //Creating a deep copy the vector
-	}
+    hand = new vector<Card>();
+    for (int i = 0; i < h.hand->size(); i++)
+    {
+        hand->emplace(hand->begin() + i, h.hand->at(i).getType()); //Creating a deep copy the vector
+    }
 }
 
 Hand::~Hand()
 {
-	delete hand; // free the memory
-	hand = NULL; // assign pointer to null
 }
 
-std::vector<Card>* Hand::getHand() // get cards of deck
+vector<Card>* Hand::getHand() // get cards of deck
 {
-	return hand;
+    return hand;
 }
 
-void Hand::setHand(std::vector<Card>* toSet)// assign a vector to cards of deck
+void Hand::setHand(vector<Card>* toSet)// assign a vector to cards of deck
 {
-	hand = toSet;
+    hand = toSet;
 }
 
 void Hand::add(Card& card) // add a card to hand
 {
-	hand->emplace_back(card); 
+    hand->emplace_back(card);
 }
 
-std::ostream& operator<<(std::ostream& ostream, const Hand& h)// print all cards in deck
+ostream& operator<<(ostream& ostream, const Hand& h)// print all cards in deck
 {
-	std::string cards = "";
-	for (int i = 0; i < h.hand->size(); i++)
-	{
-		cards += h.hand->at(i).getType() + "; ";
-	}
-	return ostream << "Hand contains the following cards: " << cards;
+    std::string cards = "";
+    for (int i = 0; i < h.hand->size(); i++)
+    {
+        cards += h.hand->at(i).getType() + "; ";
+    }
+    return ostream << "Hand contains the following cards: " << cards;
 }
+
