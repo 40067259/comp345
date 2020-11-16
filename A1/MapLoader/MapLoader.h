6f0ed1661c7d66be95d29aa4d3e6ffe83ca67365
rileyelirileyelir <69334895+rileyelirileyelir@users.noreply.github.com>
2020-11-15 19:31:02 -0500

    Update MapLoader.h

--- A1/MapLoader/MapLoader.h
+++ A1/MapLoader/MapLoader.h
@@ -11,8 +11,21 @@ using std::string;
 class MapLoader/* : public Map*/
 {
 public:
+
+    // [MapLoader default constructor]
     MapLoader();
-    Map map;
-    void loadMap(string fileInput);
+    // [MapLoader default constructor]
     
+    // [MapLoader destructor]
+    ~MapLoader();
+    // [MapLoader destructor]
+
+    // [There is a Map object as a variable in a MapLoader object]
+    Map map;
+    // [There is a Map object as a variable in a MapLoader object]    
+    
+    // [The method to load .map file into a Map object which is a variable of the MapLoader object]
+    void loadMap(string fileInput);
+    // [The method to load .map file into a Map object which is a variable of the MapLoader object]
+
 };
