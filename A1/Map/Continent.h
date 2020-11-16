1e2edde44537cebe74614d343f06ff52c2acf273
800bb <65259687+800bb@users.noreply.github.com>
2020-11-16 05:46:17 -0500

    Update Continent.h

--- A1/Map/Continent.h
+++ A1/Map/Continent.h
@@ -13,11 +13,14 @@ public:
 	Continent(std::string name, int nativeArmy);
 	~Continent();
 	int getNativeArmy() const;
+	int numOfTerritories();
 	void addTerritory(Node& Territory);
 	std::string getName() const;
 	void printTerritory();
 	//bool owner(const Player* player);
 	std::vector<Node*> getTerritories() const;
+	// check if a territory is in this continent
+	bool checkTerritory(std::string n);
 
 private:
 	int nativeArmy;
