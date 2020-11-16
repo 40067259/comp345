98720c17ab9b31733af52872c85da5ac3f856ed3
fred <flynnezhang@gmail.com>
2020-11-16 16:41:20 -0500

    cards add

---
+++ A1/Cards/Hand.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include<string>
+#include<vector>
+#include<iostream>
+#include<map>
+#include"Card.h"
+
+using namespace std;
+//predefine the classed
+class Card;
+class Deck;
+
+class Hand {
+public:
+	Hand();
+	Hand(const Hand& hand);// a copy constructor
+	~Hand(); //deconstructor because of vector instace variable
+	vector<Card>* getHand();
+	void setHand(vector<Card>* toSet);// set a vector to hand
+	void add(Card* card); // add a card to hand
+private:
+	vector<Card>* hand; // instance varibale to store cards
+	friend ostream& operator<<(ostream&, const Hand&);
+};
