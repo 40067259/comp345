f322b09ddd4bfb17acf3d7853a8b769de54244ad
rileyelirileyelir <69334895+rileyelirileyelir@users.noreply.github.com>
2020-11-17 10:58:53 -0500

    Create player.h

---
+++ A2/A2-withoutObserver/player.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <vector>
+#include "Territory.h"
+#include "OrdersList.h"
+#include "Orders.h"
+#include "Map.h"
+#include "Hand.h"
+
+class Hand;
+class Card;
+
+class Player {
+private:
+    std::vector<Territory*> _territories;
+    Hand* _cards;
+    OrdersList* _orderList;
+    std::string _name;
+    int _playerNumber;
+    int _armies = 0;
+    static int numberOfPlayers;
+
+public:
+    Player();
+    Player(std::string name);
+    Player(std::string name, std::vector<Territory*> territories, Hand* cards, OrdersList* orderlist);
+    ~Player();
+    std::vector<Territory*> arbitraryTerritoriesToAttack; //TEMPORARY: For the first assignement
+    std::vector<Territory*> toDefend();
+    std::vector<Territory*> toAttack(Territory* territoryToAttack);
+    void issueOrder(int orderType);
+    void printPlayerOrders();
+    void printPlayerTerritories();
+    void giveNewArmy(int num);
+    void addTerritory(Territory* terr);
+
+
+    std::vector<Territory*> getTerritories();
+    Hand* getHand();
+    OrdersList* getOrdersList();
+    std::string getName();
+    int getArmies();
+
+    //added
+    std::vector<Territory*> _territoriesToDefend_priority;
+    std::vector<Territory*> _territoriesToAttack_priority;
+    //added
+
+};
