ce758f0bff593c967eef36d4618bc50210895423
rileyelirileyelir <69334895+rileyelirileyelir@users.noreply.github.com>
2020-11-17 12:48:19 -0500

    Uploaded the updated Card.cpp

--- A1/Cards/Card.cpp
+++ A1/Cards/Card.cpp
@@ -12,13 +12,13 @@ Card::Card() {
 	type = "Bomb";
 }
 
- Card::Card(std::string c) {
+Card::Card(std::string c) {
 	type = c;
 }
- Card::Card(const Card& c) {
-	 std::string copytype = c.type;
-	 this->type = copytype;
- }
+
+Card::Card(const Card& card) {
+	type = card.type;
+}
 
 std::string Card::getType() {
 	return type;
@@ -30,18 +30,18 @@ std::ostream& operator<<(std::ostream& ostream, const Card& card)
 }
 
 
-void Card::play(Deck *deck,Hand *hand) {
-	std::vector<Card> *temp = hand->getHand();// get hand pointer to manage hand vector
+void Card::play(Deck* deck, Hand* hand) {
+	std::vector<Card>* temp = hand->getHand();// get hand pointer to manage hand vector
 	int size = temp->size(); // obtain the size to traverse the vector
 	for (int i = size - 1; i >= 0; i--) {// back to front so that to pop-back
-		Card *current = &(temp->at(i)); //obtain current pointer
-		std::cout << "A card is played and " << *current <<std:: endl;// display the shown card
+		Card* current = &(temp->at(i)); //obtain current pointer
+		std::cout << "A card is played and " << *current << std::endl;// display the shown card
 		std::vector<Card>* loss = hand->getHand();// obtain hand
 		std::vector<Card>* obtain = deck->getAvailableCards();// obtain deck cards
 		deck->insertCard(temp->at(i));//deck pile gets one card
 		loss->pop_back();//hand loses one card
-		
+
 	}
-	
-		
+
+
 }
