a43b88b156dfbb68f7dcbd84f9f81a590b7bff02
rileyelirileyelir <69334895+rileyelirileyelir@users.noreply.github.com>
2020-11-17 11:44:02 -0500

    updated all available version of Player.h

--- A1/Player/Player.h
+++ A1/Player/Player.h
@@ -1,51 +1,54 @@
 #pragma once
+
 #include <vector>
 #include "Territory.h"
 #include "OrdersList.h"
 #include "Orders.h"
 #include "Map.h"
 #include "Hand.h"
+#include "Card.h"
 
 class Hand;
-class Card;
+class Cards;
 
 class Player {
 private:
     std::vector<Territory*> _territories;
-    Hand *_cards;
-    OrdersList *_orderList;
-    string _name;
+    Hand* _cards;
+    OrdersList* _orderList;
+    std::string _name;
     int _playerNumber;
     int _armies = 0;
     int reinforcementPool;
     static int numberOfPlayers;
-    
+
 public:
-   Player();
-    Player(string name);
-   Player(string name, std::vector<Territory*> territories ,Hand *cards, OrdersList *orderlist);
-   ~Player();
-   std::vector<Territory*> arbitraryTerritoriesToAttack; //TEMPORARY: For the first assignement
-   std::vector<Territory*> toDefend();
-   std::vector<Territory*> toAttack();
-   void issueOrder(int orderType);
-   void printPlayerOrders();
-   void printPlayerTerritories();
-   void giveNewArmy(int num);
-   void addTerritory(Territory *terr);
+    Player();
+    Player(std::string name);
+    Player(std::string name, std::vector<Territory*> territories, Hand* cards, OrdersList* orderlist);
+    ~Player();
+    std::vector<Territory*> arbitraryTerritoriesToAttack; //TEMPORARY: For the first assignement
+    std::vector<Territory*> toDefend();
+    //std::vector<Territory*> toAttack();
+    std::vector<Territory*> toAttack(Territory* territoryToAttack);
+    void issueOrder(int orderType);
+    void printPlayerOrders();
+    void printPlayerTerritories();
+    void giveNewArmy(int num);
+    void addTerritory(Territory* terr);
     void addReinforcements(int r);
-    
-    
-    vector<Territory*> getTerritories();
+
+    std::vector<Territory*> getTerritories();
     Hand* getHand();
     OrdersList* getOrdersList();
-    string getName();
+    std::string getName();
     int getArmies();
     int getReinforcements();
-    
 
-    
-
+    //added
+    std::vector<Territory*> _territoriesToDefend_priority;
+    std::vector<Territory*> _territoriesToAttack_priority;
+    //added
 
 };
 
