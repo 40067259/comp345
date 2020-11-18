38bad182179efde9dccb81f2f2efd9ada268b956
ChouChouGrizzly <51091691+ChouChouGrizzly@users.noreply.github.com>
2020-11-18 18:50:20 -0500

    Update GameEngine.h

--- A2/A2-withoutObserver/Part 1/GameEngine.h
+++ A2/A2-withoutObserver/Part 1/GameEngine.h
@@ -2,33 +2,35 @@
 #include "Player.h"
 //#include "GameObservers.h"
 #include "MapLoader.h"
+#include "Map.h"
 #include<iostream>
 #include<string>
 #include <fstream>
 #include <cstdint>
-#include <experimental/filesystem>  // for C++14
-//#include <filesystem>  // for C++17
+#include <filesystem>
 
 class GameEngine {
 public:
-	GameEngine();
-	~GameEngine();
-	void GameStart();
-	void mapSelection();
-	void playerSelection();
+    GameEngine();
+    ~GameEngine();
+    void GameStart();
+    void mapSelection();
+    void playerSelection();
     vector<Player*> getPlayersVector();
+    void randomOrderOfPlayers();
+    void territoriesAssignment();
+    void armiesInitialization();
+    void gameStartupPhase();
 
 private:
-	std::string selectedMap;
-	int numberOfPlayers;
-	Deck* deckCards;
-	vector<Player*> playersVector;
-	Map* myMap;
-	MapLoader* mapLoader;
-	vector<string> mapNames;
-	std::string currentphase;
+    std::string selectedMap;
+    int numberOfPlayers;
+    Deck* deckCards;
+    vector<Player*> playersVector;
+    Map* myMap;
+    MapLoader* mapLoader;
+    vector<string> mapNames;
+    std::string currentphase;
 };
-
-
 
 
