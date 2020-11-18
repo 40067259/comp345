4ec97c87b2e790fbe73291b1aa7c7025e7250b82
rileyelirileyelir <69334895+rileyelirileyelir@users.noreply.github.com>
2020-11-17 19:57:05 -0500

    updated issuingOrderPhase()
    please add execute() inside each order

--- A2/A2-withoutObserver/GameEngine.cpp
+++ A2/A2-withoutObserver/GameEngine.cpp
@@ -238,5 +238,174 @@ void GameEngine::issuingOrderPhase()
 
 void GameEngine::ordersExectionPhase()
 {
-    
+    //this is a 2-way vector to wrap the order and the territory
+    std::vector<Orders*, Territory*> orderIssuedAndTerritoryMerged;
+
+    Orders* orders;
+
+    for (Player* p : playersVector)
+    {
+        //[get the order type]
+        // [Deploy]
+        if ((p->getOrdersList()->getListOfOrders().front()->orderType).compare("Deploy") == 0)
+        {
+            orders = new Deploy();
+
+            // [Deploy is for defense, so get the land from "p->_territoriesToDefend_priority"]
+            cout << "Deploy armies to " << p->_territoriesToDefend_priority.front()->getName() << "\n";
+
+            // exceute()
+
+            // [pop the begin() of p->_territoriesToDefend_priority]
+            p->_territoriesToDefend_priority.erase(p->_territoriesToDefend_priority.begin());
+            // [pop the begin() of p->_territoriesToDefend_priority]
+            
+            // The order will be pop at the end
+        }
+        // [Deploy]
+
+        // [Advance]
+        else if ((p->getOrdersList()->getListOfOrders().front()->orderType).compare("Advance") == 0)
+        {
+            int user_choice; // Let the player choose whether the player wanna send army for offense or defense
+
+            // [Advance is for both offense and defense]
+            // [Ask the user to choose between offense and defense]
+            cout << "Select a choice between the followings: " << "\n";
+            cout << "Press 1 to Attack " << p->arbitraryTerritoriesToAttack.front()->getName() << "\n";
+            cout << "OR" << "\n";
+            cout << "Press 2 to Defend " << p->_territoriesToDefend_priority.front()->getName() << "\n";
+            cin >> user_choice;
+            // [Ask the user to choose between offense and defense]
+            
+            // [Depends on the choice]
+            if (user_choice == 1)
+            {
+                // [Send army for Offense]
+                cout << "Advance: Deploy army to attack " << p->arbitraryTerritoriesToAttack.front()->getName() << "\n";
+
+                // exceute()
+
+                // [pop the items from p->arbitraryTerritoriesToAttack]
+                p->arbitraryTerritoriesToAttack.erase(p->arbitraryTerritoriesToAttack.begin());
+                // [pop the items from p->arbitraryTerritoriesToAttack]
+
+
+                // [Send army for Offense]
+            }
+            else if (user_choice == 2)
+            {
+                // [Deploy army for Defense]
+                cout << "Advance: Deploy army to defend " << p->_territoriesToDefend_priority.front()->getName() << "\n";
+
+                // [Deploy armies to the targeted player's territory]
+                // how to do it?
+                // get the source army?
+                // [Deploy armies to the targeted player's territory]
+
+                // exceute()
+
+                // [pop the items from p->_territoriesToDefend_priority]
+                p->_territoriesToDefend_priority.erase(p->_territoriesToDefend_priority.begin());
+                // [pop the items from p->_territoriesToDefend_priority]
+
+                // [Deploy army for Defense]
+            }
+            else
+            {
+                cout << "The selection is not recognized. See you next round. " << "\n";
+            }
+            // [Depends on the choice]
+
+            // The order will be pop at the end
+        }
+        // [Advance]
+
+        // [Bomb]
+        else if ((p->getOrdersList()->getListOfOrders().front()->orderType).compare("Bomb") == 0)
+        {
+            // Bomb is for offense
+            cout << "Bomb " << p->arbitraryTerritoriesToAttack.front()->getName() << "\n";
+            cout << "Half of the armies on will be removed. " << "\n";
+
+            // Half of the armies of the opponents' territory will be gone
+            // exceute()
+
+            // [pop the items from p->arbitraryTerritoriesToAttack]
+            p->arbitraryTerritoriesToAttack.erase(p->arbitraryTerritoriesToAttack.begin());
+            // [pop the items from p->arbitraryTerritoriesToAttack]
+
+        }
+        // [Bomb]
+
+        // [Blockade]
+        else if ((p->getOrdersList()->getListOfOrders().front()->orderType).compare("Blockade") == 0)
+        {
+            // Blockade is target the player's own land (Territory from p->_territoriesToDefend_priority)
+            // The number of armies on the territory is doubled
+            // The ownership of the territory is transferred to the Neutral player.
+            cout << "Blockade order is excuded. " << "\n";
+            cout << "Targeting " << p->_territoriesToDefend_priority.front()->getName();
+            cout << "The number of armies on the territory is doubled" << "\n";
+            cout << "The ownership of the territory is transferred to the Neutral player." << "\n";
+
+            // exceute()
+
+            // [pop the items from p->_territoriesToDefend_priority]
+            p->_territoriesToDefend_priority.erase(p->_territoriesToDefend_priority.begin());
+            // [pop the items from p->_territoriesToDefend_priority]
+
+        }
+        // [Blockade]
+
+        // [Airlift]
+        else if ((p->getOrdersList()->getListOfOrders().front()->orderType).compare("Airlift") == 0)
+        {
+            // Airlift is for offense
+            // The selected number of armies is attacking that territory
+            // Armies will be taken from the player's territory to the targeted territory
+            cout << "The selected number of armies is attacking "<< p->arbitraryTerritoriesToAttack.front()->getName() << "\n";
+
+            // exceute()
+
+            // [pop the items from p->arbitraryTerritoriesToAttack]
+            p->arbitraryTerritoriesToAttack.erase(p->arbitraryTerritoriesToAttack.begin());
+            // [pop the items from p->arbitraryTerritoriesToAttack]
+
+        }
+        // [Airlift]
+
+        // [Negotiate]
+        else if ((p->getOrdersList()->getListOfOrders().front()->orderType).compare("Negotiate") == 0)
+        {
+            // Negotiate targets opponents' territory
+            // The effect is that any attack that may be declared between territories of the player issuing the negotiate orderand the target player will result in an invalid order.
+
+            cout << "Negotiate with " << p->arbitraryTerritoriesToAttack.front()->getName() << "'s Owner: " << p->arbitraryTerritoriesToAttack.front()->getOwner()->getName() << "\n";
+
+            // exceute()
+
+            // [pop the items from p->arbitraryTerritoriesToAttack]
+            p->arbitraryTerritoriesToAttack.erase(p->arbitraryTerritoriesToAttack.begin());
+            // [pop the items from p->arbitraryTerritoriesToAttack]
+
+        }
+        // [Negotiate]
+
+        else
+        {
+        cout << "Error" << "\n";
+        }
+        //[get the order type]
+
+        //[Pop the order out from the orderslist vector]
+        p->getOrdersList()->getListOfOrders().erase(p->getOrdersList()->getListOfOrders().begin());
+        //[Pop the order out from the orderslist vector]
+
+    }
+}
+
+bool GameEngine::ownsContinent()
+{
+    return true;
 }
