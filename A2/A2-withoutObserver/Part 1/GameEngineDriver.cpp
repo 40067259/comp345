213c7f2944ab6e8b2a05fc413d1e3eb17b28dad6
800bb <65259687+800bb@users.noreply.github.com>
2020-11-18 21:13:25 -0500

    Add files via upload

---
+++ A2/A2-withoutObserver/Part 1/GameEngineDriver.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "GameEngine.h"
+
+int main() {
+	//This is a test driver that demonstrates 
+	// Part 1
+	//1) different valid maps can be loaded and their validity is verified and invalid maps are gracefully rejected; 
+	//2) the right number of players is created, a deck with the right number of cards is created; 
+	//3) the observers can be turned on / off during the game start phase.
+
+	GameEngine *game = new GameEngine();
+	game->GameStart();
+
+	// Part 2
+	// 1) all territories in the map have been assigned to one and only one player
+	game->startupPhase();
+	game->gameMap->printMapInfo();
+	// 2) Players are given a number of armies;
+	int num = game->getNbOfPlayers();
+	for (int i = 0; i < num; i++){
+		std::cout<<"Player "<<game->players.at(i)->getName()<<" owns ";
+		std::cout << game->players.at(i)->getReinforcements() <<" armies."<<std::endl;
+	}
+	// 3) all players have all the orders for playing in a turn.
+	std::cout << "The order of players in game:" << std::endl;
+	for (int i = 0; i < game->players.size(); i++) {
+		std::cout << " Player " << game->players.at(i)->getName() << " : "<< i << std::endl;
+	}
+	int a;
+	cin >> a;
+
+	return 0;
+
+}
