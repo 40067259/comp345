8bf4db5532d400e1a16ee6c1420a198d58b425f1
rileyelirileyelir <69334895+rileyelirileyelir@users.noreply.github.com>
2020-11-27 02:52:32 -0500

    Create PlayerStrategy.cpp

---
+++ A2/A2-withoutObserver/Part 1/PlayerStrategy/PlayerStrategy.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "PlayerStrategy.h"
+
+class PlayerStrategy
+{
+
+};
+
+class HumanPlayerStrategy : public PlayerStrategy
+{
+
+};
+
+class AggressivePlayerStrategy : public PlayerStrategy
+{
+
+};
+
+class BenevolentPlayerStrategy : public PlayerStrategy
+{
+
+};
+
+class NeutralPlayerStrategy : public PlayerStrategy
+{
+
+};
