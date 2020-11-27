ba4e94ba8a74ef0ed2b5a0305e5c9283e493e155
rileyelirileyelir <69334895+rileyelirileyelir@users.noreply.github.com>
2020-11-27 02:57:20 -0500

    Update PlayerStrategy.h

--- A2/A2-withoutObserver/Part 1/PlayerStrategy/PlayerStrategy.h
+++ A2/A2-withoutObserver/Part 1/PlayerStrategy/PlayerStrategy.h
@@ -6,25 +6,29 @@
 
 class PlayerStrategy
 {
-
+	PlayerStrategy();
 };
 
 class HumanPlayerStrategy : public PlayerStrategy
 {
-
+	HumanPlayerStrategy();
+	~HumanPlayerStrategy();
 };
 
 class AggressivePlayerStrategy : public PlayerStrategy
 {
-
+	AggressivePlayerStrategy();
+	~AggressivePlayerStrategy();
 };
 
 class BenevolentPlayerStrategy : public PlayerStrategy
 {
-
+	BenevolentPlayerStrategy();
+	~BenevolentPlayerStrategy();
 };
 
 class NeutralPlayerStrategy : public PlayerStrategy
 {
-
+	NeutralPlayerStrategy();
+	~NeutralPlayerStrategy();
 };
