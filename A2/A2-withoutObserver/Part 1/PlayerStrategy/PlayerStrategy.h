6e578c3dfa8ba7f4cfaefe85cdc5462512edc561
rileyelirileyelir <69334895+rileyelirileyelir@users.noreply.github.com>
2020-11-27 02:52:51 -0500

    Create PlayerStrategy.h

---
+++ A2/A2-withoutObserver/Part 1/PlayerStrategy/PlayerStrategy.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Player.h"
+
+class PlayerStrategy
+{
+
+};
+
+class HumanPlayerStrategy : public PlayerStrategy
+{
+
+};
+
+class AggressivePlayerStrategy : public PlayerStrategy
+{
+
+};
+
+class BenevolentPlayerStrategy : public PlayerStrategy
+{
+
+};
+
+class NeutralPlayerStrategy : public PlayerStrategy
+{
+
+};
